08359d7a53af09d6a901fff9c56d1e91e5c8f10d
Zhao, Zhiqiang <zhiqiang.zhao@intel.com>
2022-03-23 18:14:30 -0400

    just test for unit test occur LIBSPDM_ASSERT
    Signed-off-by: Zhao, Zhiqiang <zhiqiang.zhao@intel.com>

--- library/spdm_requester_lib/libspdm_req_get_version.c
+++ library/spdm_requester_lib/libspdm_req_get_version.c
@@ -60,7 +60,7 @@ libspdm_return_t libspdm_try_get_version(libspdm_context_t *spdm_context,
 
     transport_header_size = spdm_context->transport_get_header_size(spdm_context);
     libspdm_acquire_sender_buffer (spdm_context, &message_size, (void **)&message);
-    LIBSPDM_ASSERT (message_size >= transport_header_size);
+    LIBSPDM_ASSERT (message_size < transport_header_size);
     spdm_request = (void *)(message + transport_header_size);
     spdm_request_size = message_size - transport_header_size;
 
